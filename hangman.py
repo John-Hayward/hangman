7a1362b5cc8fea48e9c419fcf0b7d190b03a3c98
John <haywardjohn11@gmail.com>
2022-08-24 21:13:48 +0100

    Added tracker for previously guessed letters

--- hangman.py
+++ hangman.py
@@ -10,6 +10,7 @@ target_arr=[]
 for letter in target_word:
     target_arr.append(letter)
 guessed_arr=["_"]*len(target_word)
+guessed_letters=[]
 
 
 lives=10
@@ -25,18 +26,26 @@ print(" ".join(guessed_arr) + "\n")
 print(f"Starting lives: {lives} \n")
 
 while lives>0:
-    current_guess=input("Guess a letter ").strip().lower()
+    current_guess=input("Guess a letter: ").strip().lower()
 
     if len(current_guess)!=1:
-        print("Invalid guess, please enter one character")
+        print("Invalid guess, please enter one character \n")
         pass
 
     guess(current_guess,target_arr,guessed_arr)
 
-    if not guess(current_guess,target_arr,guessed_arr):
-        lives -=1 
+    if not guess(current_guess,target_arr,guessed_arr) and current_guess not in guessed_letters:
+        lives -=1
+
+    if current_guess in guessed_letters:
+        print("\n Letter already guessed")
+    else:
+        guessed_letters.append(current_guess)
+
 
     print(" ".join(guessed_arr) + "\n")
+    guessedstring=",".join(sorted(guessed_letters))
+    print(f"Already Guessed Letters: {guessedstring} \n")
     print(f"Remaining lives: {lives} \n")
 
     if guessed_arr==target_arr:
