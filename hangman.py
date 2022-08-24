a2e651a1c6418eee9ce363e606585680b4a9a988
John <haywardjohn11@gmail.com>
2022-08-24 20:19:50 +0100

    Added arrays for guessed and target, and function for making a guess

--- hangman.py
+++ hangman.py
@@ -1,1 +1,17 @@
 print("Hangman Game")
+
+target_word="rhythm"
+target_arr=[]
+for letter in target_word:
+    target_arr.append(letter)
+guessed_arr=["_"]*len(target_word)
+lives=10
+
+def guess(letter, target_array, guessed_array):
+    for i in range(len(target_array)):
+        if target_array[i]==letter:
+            guessed_array[i]=letter
+
+print(" ".join(guessed_arr))
+guess("r",target_arr,guessed_arr)
+print(" ".join(guessed_arr))
